702f83298d48931e3033d7fcff6ad9f0f071b2fd
LouisDigit <louisv.digit@gmail.com>
2021-11-17 15:01:46 +0100

    start rotation

--- src/modele/main.c
+++ src/modele/main.c
@@ -58,14 +58,14 @@ char *color(Cellule c)
     }
 }
 
-void printFace(Face face)
+void printFace(Face *face)
 {
     int i, j;
     for (i = 0; i < TAILLE_MATRICE; i++)
     {
         for (j = 0; j < TAILLE_MATRICE; j++)
         {
-            printf("%s ", printCellule(face.tab[i][j]));
+            printf("%s ", printCellule(face->tab[i][j]));
         }
         printf("\n");
     }
@@ -75,7 +75,7 @@ void printRubikube(Face *rubikube)
 {
     for (int i = 0; i < NB_FACE; i++)
     {
-        printFace(rubikube[i]);
+        printFace(&rubikube[i]);
     }
 }
 
@@ -169,6 +169,59 @@ void printTerminalRubikCube(Face *rubikube[])
     printTerminalFace(rubikube[BAS]);
 }
 
+Face *getFace(enum Direction dir,Face *rubikube[])
+{
+  switch (dir)
+  {
+    case HAUT:
+      return &rubikube[0];
+      break;
+    case BAS:
+       return &rubikube[5];
+      break;
+    case GAUCHE:
+       return &rubikube[1];
+      break;
+    case DROITE:
+       return &rubikube[3];
+      break;
+    case DEVANT:
+       return &rubikube[2];
+      break;
+    case DERRIERE:
+       return &rubikube[4];
+      break;
+  }
+}
+
+void setFace(Face *face,enum Direction dir,Face *rubikube[])
+{
+  for (int i = 0; i < 3;i++)
+  {
+    for (int j = 0; j < 3; j++)
+    {
+      face->tab[i][j].face = getFace(dir,rubikube);
+    }
+  }
+}
+
+void rotateLigneHaut(Face *rubikube[])
+{
+  Face *avant = getFace(DEVANT,rubikube);
+  Face *derrier = getFace(DERRIERE,rubikube);
+  Face *gauhe = getFace(GAUCHE,rubikube);
+  Face *droite = getFace(DROITE,rubikube);
+  Face *haut = getFace(HAUT,rubikube);
+  Face *bas = getFace(BAS,rubikube);
+
+
+  // Face avant -> Face droite
+  droite->tab[0][0] = avant->tab[0][0];
+  droite->tab[0][1] = avant->tab[0][1];
+  droite->tab[0][2] = avant->tab[0][2];
+  setFace(droite,DROITE,rubikube);
+}
+
 
 
 int main(int argc, char *argv[])
@@ -176,6 +229,8 @@ int main(int argc, char *argv[])
     Face *rubikube[NB_FACE];
     creerRubikube(rubikube);
     // printRubikube(rubikube);
+    printTerminalRubikCube(rubikube);
+    rotateLigneHaut(rubikube);
     printTerminalRubikCube(rubikube);
     return 0;
 }
--- src/modele/methode.c
+++ src/modele/methode.c
@@ -1,13 +1,57 @@
 #include <stdio.h>
 #include "struct.h"
 
-void permuteCellule(Cellule *c1,Cellule *c2)
+Face *getFace(enum Direction dir,Face *rubikube)
 {
-  Cellule *c3;
-  c3 = c1;
-  c1 = c2;
-  c1->face = c3.face;
+  switch (dir)
+  {
+    case HAUT:
+      return &rubikube[0];
+      break;
+    case BAS:
+       return &rubikube[5];
+      break;
+    case GAUCHE:
+       return &rubikube[1];
+      break;
+    case DROITE:
+       return &rubikube[3];
+      break;
+    case DEVANT:
+       return &rubikube[2];
+      break;
+    case DERRIERE:
+       return &rubikube[4];
+      break;
+  }
+}
 
+void setFace(Face *face,enum Direction dir,Face *rubikube)
+{
+  for (int i = 0; i < 3;i++)
+  {
+    for (int j = 0; j < 3; j++)
+    {
+      face->tab[i][j].face = getFace(dir,rubikube);
+    }
+  }
+}
+
+void rotateLigneHaut(Face *rubikube)
+{
+  Face *avant = getFace(DEVANT,rubikube);
+  Face *derrier = getFace(DERRIERE,rubikube);
+  Face *gauhe = getFace(GAUCHE,rubikube);
+  Face *droite = getFace(DROITE,rubikube);
+  Face *haut = getFace(HAUT,rubikube);
+  Face *bas = getFace(BAS,rubikube);
+
+
+  // Face avant -> Face droite
+  droite->tab[0][0] = avant->tab[0][0];
+  droite->tab[0][1] = avant->tab[0][1];
+  droite->tab[0][2] = avant->tab[0][2];
+  setFace(droite,DROITE,rubikube);
 }
 
 int main(void)
