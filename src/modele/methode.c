fd3d6b78f53bcd2ea3fbeb5ad2e999c83cdfd7c8
LEBLOND Paul <polo62.leblond@gmail.com>
2021-11-17 15:11:26 +0100

    conflict

--- src/modele/methode.c
+++ src/modele/methode.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 #include "struct.h"
 
-void permuteCellule(Cellule *c1,Cellule *c2)
+void permuteCellule(Cellule *c1, Cellule *c2)
 {
   Cellule *c3;
   c3 = c1;
   c1 = c2;
   c1->face = c3.face;
-
 }
 
 int main(void)
